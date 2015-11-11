c0780f3ef0c329af21bd35e976e9e690ce6ed7b9
Ryan Zhou <ryan.gm.nb@gmail.com>
2015-11-11 15:57:30 -0500

    Enable the leaveContact fn

--- lulu/urls.py
+++ lulu/urls.py
@@ -21,7 +21,7 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        #url(r'^static/(?P<path>/*)$', 'django.views.static.serve',
                        #    {'document_root': 'C:\Users\Ryan\Desktop\lulu_new\introPage'}),
-                       #url(r'^leaveContactInfo/$', 'introPage.views.leaveContactInfo'),
+                       url(r'^leaveContactInfo/$', 'introPage.views.leaveContactInfo'),
                        #url(r'^test/$', 'singleSlot.views.singleSlotStep'),
                        #url(r'^realtest/$','singleSlot.views.test'),
 )
