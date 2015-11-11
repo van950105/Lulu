e7547a0022d32ac30aba4be88e70baa4800a836f
root <root@lulu.c.lulu-today.internal>
2015-11-11 21:00:24 +0000

    Change in wsgi

--- lulu/wsgi.py
+++ lulu/wsgi.py
@@ -9,9 +9,35 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lulu.settings")
+
+#application = get_wsgi_application()
+
+#import os
+import sys
+import site
+root =os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0,root)
+
+packages = os.path.join(root,'lulu.today/lib/python2.7/site-packages')
+sys.path.insert(0,packages)
+
+#add the site-packages of the chosen virtualenv to work with
+site.addsitedir(packages)
+#add the app's directory to the PYTHONPATH
+sys.path.append('/home/Lulu/lulu')
+sys.path.append('/home/Lulu/lulu/lulu')
+#add side packages for virtual envs
+#sys.path.append('/.virtualenvs/Lib/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'lulu.settings'
+
+
+#Activate your virtual env
+activate_env = os.path.join(root,'lulu.today/bin/activate_this.py')
+
+execfile(activate_env, dict(__file__ = activate_env))
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lulu.settings")
-
 application = get_wsgi_application()
-
